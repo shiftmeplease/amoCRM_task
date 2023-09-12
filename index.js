3072f429e759dd08dbee7f7262922252207cca75
inferno vps <noname@mail.ru>
2023-09-12 01:41:26 +0000

    Express adjustments

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 import express from "express";
 import { handleContact } from "./controllers.js";
 
-const port = 3000;
+const port = 3030;
 const app = express();
+
+app.use((err, req, res, next) => {
+  const { ips, params, route } = req;
+  console.log({ ips, params, route });
+  res.status(500).send({ error: err.message });
+});
 
 app.get("/", async (req, res, next) => {
   const { query } = req;
+  if (Object.keys(query).length === 0)
+    return res.send("https://github.com/shiftmeplease/amoCRM_task");
+
   try {
-    if (!query) throw new Error("No query params");
     const { name, email, phone } = query;
+
     if (!name) throw new Error("No name parameter");
     if (!email) throw new Error("No email parameter");
     if (!phone) throw new Error("No phone parameter");
 
     const result = await handleContact(query.name, query.phone, query.email);
-    res.send(result);
+    res.status(200).send(result);
   } catch (e) {
     return next(e);
   }
@@ -31,6 +40,7 @@ app.get("/", async (req, res, next) => {
 
 app.use((err, req, res, next) => {
   console.log(err);
+  //TODO fit status code to error type/message
   res.status(500).send({ error: err.message });
 });
 
